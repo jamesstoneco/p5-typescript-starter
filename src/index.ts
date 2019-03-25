a50ccf8a0513eba28bc9c2e349b1f6d1cfcbcd09
James Stone <james.stone@futurice.com>
2019-03-25 07:40:14 +0200

    updated wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import * as p5 from "p5";
-import * as gl from "gl-matrix";
 import * as R from "ramda";
-import map from "ramda/es/map";
+import * as PIXI from "pixi.js";
 
-const numberOfParticles = 100;
+const numberOfParticles = 10;
 
 interface Vector {
   x: number;
@@ -15,50 +13,86 @@ interface Particle {
   vel: Vector;
 }
 
+interface ParticleGraphic extends Particle {
+  g: PIXI.Graphics;
+}
+
+const stageWidth = 400;
+const stageHeight = 400;
+
 const particles: Array<Particle> = R.map(() => {
   return {
-    loc: { x: 0, y: 0 },
+    loc: {
+      x: (Math.random() - 0.5) * stageWidth,
+      y: (Math.random() - 0.5) * stageHeight
+    },
     vel: { x: Math.random() - 0.5, y: Math.random() - 0.5 }
   };
 }, Array(numberOfParticles));
 
-let newLocation: Array<Particle> = particles;
-
-const addVec = (a: Vector, b: Vector): Vector => {
-  return { x: a.x + b.x, y: a.y + b.y };
-};
-
-const sketchP = new p5((p: p5) => {
-  p.preload = () => {};
-
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-  };
-
-  p.draw = () => {
-    p.translate(p.windowWidth / 2, p.windowHeight / 2);
-    p.background(127);
-    p.noStroke();
-    p.fill(0);
-
-    newLocation = R.map(p => {
-      return { loc: addVec(p.loc, p.vel), vel: p.vel };
-    }, newLocation);
-
-    // console.log(
-    //   R.map(part => {
-    //     return { x: part.loc.x, y: part.loc.y };
-    //   }, newLocation)[0]
-    // );
-
-    R.map(particle => {
-      p.ellipse(particle.loc.x, particle.loc.y, 15, 15);
-    }, newLocation);
-  };
+let app = new PIXI.Application({
+  width: stageWidth,
+  height: stageHeight,
+  antialias: true,
+  transparent: false,
+  resolution: 1
 });
 
-export default sketchP;
+let count = 0;
+
+app.ticker.add(function() {
+  count += 0.1;
+  const moveParticle = (p: ParticleGraphic) => {
+    const { g } = p;
+    g.moveTo(g.position.x + count, g.position.y + count);
+  };
+  R.map(moveParticle, pgs);
+
+  // TODO: nothing moves?
+});
+
+const centerRect = (x: number, y: number, size: number): PIXI.Graphics => {
+  const g = new PIXI.Graphics();
+  g.beginFill(0x5cafe2);
+  g.x = app.renderer.width / 2;
+  g.y = app.renderer.height / 2;
+  g.drawRect(x + -size / 2, y + -size / 2, x + size, x + size);
+  return g;
+};
+
+document.body.appendChild(app.view);
+
+// pointfree functions to be used below
+const buildParticleGraphic = (p: Particle): ParticleGraphic => {
+  const { loc, vel } = p;
+  return { loc, vel, g: centerRect(loc.x, loc.y, 5) };
+};
+
+const addToStage = (obj: ParticleGraphic) => {
+  app.stage.addChild(obj.g);
+  return obj;
+};
+
+// R.map(addToStage, R.map(buildParticleGraphic, particles));
+
+const trace = (msg: string) => (val: any) => console.log(msg, " : ", val);
+
+const pgs = R.map(
+  R.compose(
+    R.tap(trace("end")),
+    addToStage,
+    R.tap(trace("middle")),
+    buildParticleGraphic,
+    R.tap(trace("begin"))
+  ),
+  particles
+);
+
+// so pipe is unconventional in functional languages, because it doesn't follow the mathematical model
+// R.map(
+//   R.pipe(
+//     buildParticleGraphic,
+//     addToStage
+//   ),
+//   particles
+// );
